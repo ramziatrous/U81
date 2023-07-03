1894d1bef2857d50dcd085868844e39e73160982
Ramzi Atrous <atrous.ramzi@gmx.de>
2023-07-03 23:57:19 +0200

    d

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+const axios = require ('axios');
+const apikey = 'e90eb26ee4fc91193ecd9a042e920a53';
+const stadt = process.argv[2];
+
+const apiurl = `http://api.openweathermap.org/data/2.5/forecast?q=${stadt}&appid=${apikey}`
+
+axios.get(url)
+.then(response => {
+    const weatherData = response.data;
+    const temperature = weatherData.main.temp;
+    const weatherDescription = weatherData.weather[0].description;
+    console.log('Aktuelles Wetter in ${stadt}:');
+    console.log('Temperatur ${temperature}°C');
+    console.log('Wetterberschreibung${WetherDescription}:');
+})
+.catch(error => {
+    console.log('Fehler beim Abrufen der Wetterdaten  ' , error.message);
+});
